431b923488637021c21703471fd49028cc3990d5
Olean1542 <66017122+Olean1542@users.noreply.github.com>
2021-09-08 08:27:04 +0900

    Update sase.py

--- sase.py
+++ sase.py
@@ -27,8 +27,8 @@ async def on_message(message):
         embed = discord.Embed(title="▧ 사세 명령어 ▨", description="「 니들 채팅에 사세가 친절히 대답해줍니다 」", color=0xffe400)
         embed.set_thumbnail(url="https://i.imgur.com/feD446z.png")
         embed.add_field(name="Ⅰ.사세와 대화하기", value="● (없음)",inline=False)
-        embed.add_field(name="Ⅱ.사세와 놀기", value="● ``가위바위보`` ``가위`` ``바위`` ``보`` ``주사위`` ``사다리타기``", inline=False)
-        embed.add_field(name="Ⅲ.사세의 갤러리", value="● ``사세 버팔로윙`` ``유미`` ``우주비행사 유미`` ``놀란 유미``", inline=False)
+        embed.add_field(name="Ⅱ.사세와 놀기", value="● ```가위바위보``` ```가위``` ```바위``` ```보``` ```주사위``` ```사다리타기```", inline=False)
+        embed.add_field(name="Ⅲ.사세의 갤러리", value="● ```사세 버팔로윙``` ```유미``` ```우주비행사 유미``` ```놀란 유미```", inline=False)
         embed.set_footer(text="º 명령어 앞에 '사세야'는 무조건 들어가야함")
         await message.channel.send("", embed=embed)
 
@@ -36,7 +36,7 @@ async def on_message(message):
         bot_response = random.randint(1, 3)
 
         if bot_response == 1:
-            await message.channel.send("```● 사세가 가위를 냈습니다.```")
+            await message.channel.send("``` ● 사세가 가위를 냈습니다.```")
             if message.content == "사세야 가위":
                 await message.channel.send("이걸 비기네")
             elif message.content == "사세야 바위":
@@ -44,7 +44,7 @@ async def on_message(message):
             else:
                 await message.channel.send("내가 이겼다")
         elif bot_response == 2:
-            await message.channel.send("```● 사세가 바위를 냈습니다.```")
+            await message.channel.send("``` ● 사세가 바위를 냈습니다.```")
             if message.content == "사세야 가위":
                 await message.channel.send("내가 이겼다")
             elif message.content == "사세야 바위":
@@ -52,7 +52,7 @@ async def on_message(message):
             else:
                 await message.channel.send("졌노")
         else:
-            await message.channel.send("```● 사세가 보를 냈습니다.```")
+            await message.channel.send("``` ● 사세가 보를 냈습니다.```")
             if message.content == "사세야 가위":
                 await message.channel.send("졌네")
             elif message.content == "사세야 바위":
@@ -61,7 +61,7 @@ async def on_message(message):
                 await message.channel.send("이걸 비기네")
 
     if message.content.startswith("사세야 가위바위보"):
-        embed = discord.Embed(title="▧ 사세와 가위바위보 하기 ▨", description="그냥 ``가위`` ``바위`` ``보``중에 하나를 내면 됨", color=0xffe400)
+        embed = discord.Embed(title="▧ 사세와 가위바위보 하기 ▨", description="그냥 ```가위``` ```바위``` ```보```중에 하나를 내면 됨", color=0xffe400)
         embed.set_footer(text="º 명령어 앞에 '사세야'는 무조건 들어가야함")
         await message.channel.send("", embed=embed)
 
