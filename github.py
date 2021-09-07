23ad22798b075d38c90bf71e3525547d3f8acda8
Olean1542 <66017122+Olean1542@users.noreply.github.com>
2021-09-07 11:29:05 +0900

    Update github.py

--- github.py
+++ github.py
@@ -23,16 +23,10 @@ async def on_message(message):
     if message.content == "사세야":
         await message.channel.send("왜 불러")
 
-    if message.content.startswith("사세야 뱅행"):
-        await message.channel.send("왜 시비지")
-
-    if message.content.startswith("사세야 왜 시비지"):
-        await message.channel.send("뱅행")
-
     if message.content == "사세야 명령어":
         embed = discord.Embed(title="▧ 사세 명령어 ▨", description="「 니들 채팅에 사세가 친절히 대답해줍니다 」", color=0xffe400)
         embed.set_thumbnail(url="https://i.imgur.com/feD446z.png")
-        embed.add_field(name="Ⅰ.사세와 대화하기", value="● ``뱅행`` ``왜 시비지``", inline=False)
+        embed.add_field(name="Ⅰ.사세와 대화하기", value="● (없음)",inline=False)
         embed.add_field(name="Ⅱ.사세와 놀기", value="● ``가위바위보`` ``가위`` ``바위`` ``보`` ``주사위`` ``사다리타기``", inline=False)
         embed.add_field(name="Ⅲ.사세의 갤러리", value="● ``사세 버팔로윙`` ``유미`` ``우주비행사 유미`` ``놀란 유미``", inline=False)
         embed.set_footer(text="º 명령어 앞에 '사세야'는 무조건 들어가야함")
